d33ca86903316ae521cdccd9958400d1509027f4
Kartik CHoudhary <kartikcodes26@gmail.com>
2026-06-24 22:34:09 +0530

    Added more notes

---
+++ Notes/OOPS/basics.py
@@ -0,0 +1,48 @@
+class vehicle:
+    def __init__(self, name: str, range: int) -> None:
+        self.range = range
+        self.name = name
+        self.is_started = False
+        
+    def start(self) -> None:
+        if self.is_started:
+            print("The vechicle is already running")
+        else:
+            self.is_started = True
+            print("The vehicle sucessfully started")
+        
+    def turn_off(self) -> None:
+        if self.is_started == False:
+            print("The car is already turned off")
+            
+        else:
+            self.is_started = False
+            print("Sucessfully turned off the car")
+            
+    # Dunder methods
+    def __add__(self, other):
+        print(f"Total range : {self.range + other.range}")
+        
+    def __mul__(self, other):
+        ...
+         
+    def __str__(self) -> str:
+        return f"Name : {self.name} \nRange : {self.range}"
+    
+    def __REPR__(self) -> str:
+        ...
+        
+        
+    
+car = vehicle("car", 120)
+print(car.name)
+print(car.range)
+car.start()
+car.start()
+car.turn_off()
+car.turn_off()
+truck = vehicle("Truck", 80)
+print(truck + car)
+print(car)
+
+  
